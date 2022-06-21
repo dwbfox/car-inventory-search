6a165614066a2eccd309400d0f4b7641b073c886
Dagmawi Biru <dag.private@protonmail.com>
2022-06-21 11:17:06 -0500

    Reworked plugin structure to support additional dealer platforms

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 #!/usr/bin/env python3
 
-from dealer import DealerInventory
 import argparse
+from urllib.parse import urlsplit
+from requests import get
+
+from dealerplugins import (
+     dealeron,
+     dealertrack
+)
+
+
+
+def load_dealer_plugin(dealer_url):
+    '''Try to detect the platform of the given dealer
+    for correct parsing'''
+
+    platforms = {
+        'dealeron': dealeron,
+        'dealertrack': dealertrack
+    }
+    res = get(dealer_url)
+    if res.status_code != 200:
+        raise Exception("Unable to determine the dealer platform. Maybe it's not supported")
+    if 'dealertrack' in res.text.lower():
+        platform = 'dealertrack'
+    elif 'dealeron' in res.text.lower():
+        platform = 'dealeron'
+    return platforms[platform]
 
 def main():
-
+    '''Main entry point'''
     parser = argparse.ArgumentParser(description='dealer-inventory-check - Get dealer inventory listing and prices without the hassle.')
     parser.add_argument('--dealer_url', required=True, metavar='dealer_url', type=str,
                         help='URL to the dealership homepage.')
@@ -13,16 +38,8 @@ def main():
     parser.add_argument('--output', default='table', choices=['csv', 'json', 'table'],
                         help='Format of the output, either csv, json, or table.')
     args = parser.parse_args()
-   
-    dealer = DealerInventory(args.dealer_url)
-    if args.output == 'json':
-        print(dealer.gen_json())
-    elif args.output == 'csv':
-        file_name = 'inventory.csv'
-        print(f"Generated CSV as {file_name}.")
-        dealer.gen_csv(file_name)
-    elif args.output == 'table':
-        print(dealer.gen_table())
+    dealer_plugin = load_dealer_plugin(args.dealer_url)
+
     
 if __name__ == '__main__':
     main()
